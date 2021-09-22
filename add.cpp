016b40c14fc9183540ef85aaadb98fb9156d8dce
Kushal Singh <69754553+kushalsinghse@users.noreply.github.com>
2021-09-22 12:43:17 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+
+    int num1, num2;
+
+    cout<<"Enter first number"<<endl;
+
+    cin>>num1;
+    cout<<"Enter second number"<<endl;
+    cin>>num2;
+    cout<<"The sum is "<<num1+num2<<endl;
+    return 0;
+}
