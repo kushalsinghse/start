ed99ae2f414f7be1ea7d7ecd8ebdcd48f4481502
Kushal Singh <69754553+kushalsinghse@users.noreply.github.com>
2021-09-22 10:30:25 +0530

    Create loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+for (int i = 0; i < 101; i++)
+{
+      cout<<i<<endl;
+}
+    int i=1;
+while (i<=100)
+{
+   cout<<i<<endl;
+   i++;
+}
+
+
+
+    return 0;
+}
